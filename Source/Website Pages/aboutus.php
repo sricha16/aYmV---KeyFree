f60c6b69a231fc718793076c3e364a1371f3325e
gernMatt <mgern@uwyo.edu>
2014-12-11 12:48:16 -0700

    Finishing Website Upload
    These commits represent work by all team members, I just made a few
    changes and made the commit

--- Source/Website Pages/aboutus.php
+++ Source/Website Pages/aboutus.php
@@ -7,6 +7,14 @@
 	<head>
 		<?php include 'style.php';?>
 
-		<p class="text">we are interesting</p><br>
+		<p class = "text"> 
+			<strong> <font size = "4"> What is Key-Free? </font> </strong> 
+			<br>
+			 Key-Free is a secure and reliable way to store passwords or any sensitive information that is hard to remember but shouldn't be written down. It is a compact micro-controller with an audio adapter that communicates via the headphone jack to store information on a micro SD card. Informaton is accessed and stored through a web portal that utilizes JavaScript to encrypt the sensitive information. That way, the user only has to remember one key, rather than all of their passwords. This can also allow users to use more secure passwords, that are longer and composed of random characters, and not worry about forgetting them. Basically, it is one password to rule them all!</p><br>
+			 
+		<p class="text"> 
+		<strong> <font size = "4"> Contact us! </font> </strong> <br>
+		stand@key-free.co
+		</p><br>
 	</head>
 </html>
