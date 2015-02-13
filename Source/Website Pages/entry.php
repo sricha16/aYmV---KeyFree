bf4557063f3280881d55a9a0e2d09ebc9430b038
fantasmita <taylormlegg@yahoo.com>
2015-02-13 16:44:36 -0700

    Web update: converting encrypted data to DTMF
    tones are played after being stored on entry.php

--- Source/Website Pages/entry.php
+++ Source/Website Pages/entry.php
@@ -13,20 +13,8 @@
 		<script src="crypto-js/sha256.js"></script>
 		<script src="jquery-2.1.1.min.js"></script>
 		
+		
 		<script>
-			// variables
-			var leftchannel = [];
-			var rightchannel = [];
-			var recorder = null;
-			var recording = false;
-			var recordingLength = 0;
-			var volume = null;
-			var audioInput = null;
-			var sampleRate = 44100;
-			var audioContext = null;
-			var context = null;
-			var outputElement = document.getElementById('output');
-			var outputString;
 			
 			// feature detection 
 			if (!navigator.getUserMedia)
@@ -56,216 +44,56 @@
 				//get description
 				var description = $('#description').val();
 				//output inputs and ciphertext
-				$('#stored').html('Information successfully stored for ' + description +' as ' + CryptoJS.enc.Base64.stringify(iv) + ciphertext);
+				var storage = CryptoJS.enc.Base64.stringify(iv) + ciphertext;
+				$('#stored').html('Information successfully stored for ' + description +' as ' + storage);
 				
-				var audio = dataToAudio(description, ciphertext, iv);
-				dataFromAudio(audio);
+				var hexVal = asciiToHex(storage);
+				$('#hexVal').html('hex of storage ' + hexVal);
+				var cleanedHexVal = hexCleanup(hexVal);
+				$('#cleanedHexVal ').html('cleanedHexVal of storage ' + cleanedHexVal );
 				
+				genDialTones(cleanedHexVal, 0);
 				
 			}			
 			
-			function dataToAudio(description, ciphertext, iv )
+			function asciiToHex(storage)
 			{
-			
-				var data = [],
-					sampleRateHz = 44100,
-					storedData = [description, ciphertext,iv],
-				
-					baseFreq = function(index){
-						var r = 2*Math.PI * 440 * Math.pow(2,(storedData[index]-69)/12.0) / sampleRateHz;
-						return r;
-					};
-				for(var j=0; j<2*sampleRateHz; j++){
-					var l = 2*sampleRateHz / storedData.length;
-					data[j] = 64 + 32 * Math.round(Math.sin(baseFreq(Math.round(j/l))*j));
+				var hex = "";
+				for (a = 0; a < storage.length; a++) {
+					//gets the unicode value of the character & converts to hex
+				    hex+=(storage.charCodeAt(a).toString(16));
 				}
-				alert('data length');
-				alert(data.length);
-				var wave = new RIFFWAVE();
-				wave.header.sampleRate = sampleRateHz;
-				wave.header.numChannels = 1;
-				wave.Make(data);
+				return hex;
+			}
+			
+			function hexCleanup (hexVal)
+			{
+				hexVal = hexVal.replace(/e/g,'#');
+				hexVal = hexVal.replace(/f/g,'*');
+				return hexVal ;
+			}
+			
+			function genDialTones(cleanedHexVal, num)
+			{
 				var audio = new Audio();
-				audio.src = wave.dataURI;
-				audio.textContent = storedData;
-				return audio;
-				/*var data = []; // just an array
-				data[0] = description;
-				data[1] = ciphertext;
-				data[2] = iv;
-				var wave = new RIFFWAVE(data); // create the wave file
-				var audio = new Audio(wave.dataURI); // create the HTML5 audio element
-				audio.textContent = wave.data;
-				return audio
-				*/
-					
+				var filename = cleanedHexVal.charAt(num);
+				if(filename == "#")
+					filename = "pound";
+				else if (filename == "*")
+					filename = "star";	
+				audio.src = filename + ".mp3";
+				audio.play();
+				audio.onended = function(){playNext(cleanedHexVal, num)};
+				
 			}
-			
-			function dataFromAudio(audio)
+			function playNext(cleanedHexVal, num)
 			{
-				startRecording();
-				//alert('playing audio');
-				audio.play();	
-				//alert('audio over');			
-				audio.onended = function(){buildWav()};
-				//alert('returned from buildwav');
-				//sleep(1000);
-				//buildWav();															
-				$('#audioResult').html('Pulling information back out of audio: ' + audio.textContent);				
-				$('#hooray').html('format of above is description, encoded data without IV, hex representation of IV (which is base64)');
+				if(num+1 < cleanedHexVal.length)
+					genDialTones(cleanedHexVal, num+1);
 			}
 			
-			function startRecording()
-			{				
-				//alert('starting to record');
-				recording = true;			
-			        // reset the buffers for the new recording
-			        leftchannel.length = rightchannel.length = 0;			        
-			        recordingLength = 0;
-			        //outputElement.innerHTML = 'Recording now...';
-			}
-			
-			function buildWav()
-			{
-				// we stop recording
-			        recording = false;
-			        //alert('recording done');
-			        
-			        //outputElement.innerHTML = 'Building wav file...';
-			
-			        // we flat the left and right channels down
-			        var leftBuffer = mergeBuffers ( leftchannel, recordingLength );
-			        var rightBuffer = mergeBuffers ( rightchannel, recordingLength );
-			        // we interleave both channels together
-			        var interleaved = interleave ( leftBuffer, rightBuffer );
-			        
-			        // we create our wav file
-			        var buffer = new ArrayBuffer(44 + interleaved.length * 2);
-			        var view = new DataView(buffer);
-			        
-			        // RIFF chunk descriptor
-			        writeUTFBytes(view, 0, 'RIFF');
-			        view.setUint32(4, 44 + interleaved.length * 2, true);
-			        writeUTFBytes(view, 8, 'WAVE');
-			        // FMT sub-chunk
-			        writeUTFBytes(view, 12, 'fmt ');
-			        view.setUint32(16, 16, true);
-			        view.setUint16(20, 1, true);
-			        // stereo (2 channels)
-			        view.setUint16(22, 2, true);
-			        view.setUint32(24, sampleRate, true);
-			        view.setUint32(28, sampleRate * 4, true);
-			        view.setUint16(32, 4, true);
-			        view.setUint16(34, 16, true);
-			        // data sub-chunk
-			        writeUTFBytes(view, 36, 'data');
-			        view.setUint32(40, interleaved.length * 2, true);
-			        
-			        // write the PCM samples
-			        var lng = interleaved.length;
-			        var index = 44;
-			        var volume = 1;
-			        for (var i = 0; i < lng; i++){
-			            view.setInt16(index, interleaved[i] * (0x7FFF * volume), true);
-			            index += 2;
-			        }
-			        
-			        // our final binary blob
-			        var blob = new Blob ( [ view ], { type : 'audio/wav' } );
-			        
-			        // let us save it locally
-			        //outputElement.innerHTML = 'Handing off the file now...';
-			        var url = (window.URL || window.webkitURL).createObjectURL(blob);
-			        var link = window.document.createElement('a');
-			        link.href = url;
-			        link.download = 'output.wav';
-			        var click = document.createEvent("Event");
-			        click.initEvent("click", true, true);
-			        link.dispatchEvent(click);
-			}
-			
-			function interleave(leftChannel, rightChannel)
-			{
-				var length = leftChannel.length + rightChannel.length;
-				var result = new Float32Array(length);
-				
-				var inputIndex = 0;
-				
-				for (var index = 0; index < length; )
-				{
-					result[index++] = leftChannel[inputIndex];
-				    	result[index++] = rightChannel[inputIndex];
-				    	inputIndex++;
-				}
-				return result;
-			}
-				
-			function mergeBuffers(channelBuffer, recordingLength)
-			{
-				var result = new Float32Array(recordingLength);
-				var offset = 0;
-				var lng = channelBuffer.length;
-				for (var i = 0; i < lng; i++)
-				{
-					var buffer = channelBuffer[i];
-				    	result.set(buffer, offset);
-				    	offset += buffer.length;
-				}
-				return result;
-			}
-				
-			function writeUTFBytes(view, offset, string)
-			{ 
-				var lng = string.length;
-				for (var i = 0; i < lng; i++)
-				{
-					view.setUint8(offset + i, string.charCodeAt(i));
-				}
-			}
-				
 			function success(e)
 			{
-				// creates the audio context
-				audioContext = window.AudioContext || window.webkitAudioContext;
-				context = new audioContext();
-				
-				console.log('succcess');
-				   
-				// creates a gain node
-				volume = context.createGain();
-				
-				// creates an audio node from the microphone incoming stream
-				audioInput = context.createMediaStreamSource(e);
-				
-				// connect the stream to the gain node
-				audioInput.connect(volume);
-				
-				/* From the spec: This value controls how frequently the audioprocess event is 
-				dispatched and how many sample-frames need to be processed each call. 
-				Lower values for buffer size will result in a lower (better) latency. 
-				Higher values will be necessary to avoid audio breakup and glitches */
-				var bufferSize = 2048;
-				recorder = context.createScriptProcessor(bufferSize, 2, 2);
-				//recorder = context.createJavaScriptNode(buffersize, 2, 2);
-				
-				recorder.onaudioprocess = function(e)
-				{
-				    //alert('loop forever!');
-				    if (!recording) return;
-				    var left = e.inputBuffer.getChannelData (0);
-				    var right = e.inputBuffer.getChannelData (1);
-				    // we clone the samples
-				    leftchannel.push (new Float32Array (left));
-				    rightchannel.push (new Float32Array (right));
-				    recordingLength += bufferSize;
-				    console.log('recording');				   
-				    //alert('recorded something');
-				}
-				
-				
-				// we connect the recorder
-				volume.connect (recorder);
-				recorder.connect (context.destination); 
 			}
 											
 		</script>
@@ -282,8 +110,8 @@
 			<input type="password" class="input-box" id="password" placeholder="Password"></input><br>
 			<button class="button-style" onclick="encrypt();">Store</button><br>
 			<p id="stored" class="message"></p><br>
-			<p id="audioResult" class="message"></p><br>		
-			<p id="hooray" class="message"></p><br>
+			<p id="hexVal" class="message"></p><br>
+			<p id="cleanedHexVal" class="message"></p><br>
 		</p>
 		<p class = "text"> 
 			Instructions for verifying Password go here
