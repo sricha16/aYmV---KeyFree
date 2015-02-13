8728de131120c9e1c6b9644b426926bc7817cac7
sricha16 <s.richard990@gmail.com>
2015-02-13 10:50:12 -0700

    Added Start Stop Signal
    Added a signal to signify when the teensy should start and stop
    listening for dial tones
    -done as a group

--- Source/Teensy_Sketches/DialTone_EncDec/DialTone_EncDec.ino
+++ Source/Teensy_Sketches/DialTone_EncDec/DialTone_EncDec.ino
@@ -30,6 +30,7 @@ AudioAnalyzeToneDetect   column1;
 AudioAnalyzeToneDetect   column2;
 AudioAnalyzeToneDetect   column3;
 AudioAnalyzeToneDetect   column4;
+AudioAnalyzeToneDetect   startStop;
 AudioSynthWaveformSine   sine1;    // 2 sine waves
 AudioSynthWaveformSine   sine2;    // to create DTMF
 AudioMixer4              mixer;
@@ -44,6 +45,8 @@ AudioConnection patchCord04(audioIn, 0, row4, 0);
 AudioConnection patchCord05(audioIn, 0, column1, 0);
 AudioConnection patchCord06(audioIn, 0, column2, 0);
 AudioConnection patchCord07(audioIn, 0, column3, 0);
+AudioConnection patchCord08(audioIn, 0, column4, 0);
+AudioConnection patchCord09(audioIn, 0, startStop, 0);
 AudioConnection patchCord10(sine1, 0, mixer, 0);
 AudioConnection patchCord11(sine2, 0, mixer, 1);
 AudioConnection patchCord12(mixer, 0, audioOut, 0);
@@ -80,10 +83,10 @@ void setup() {
   audioShield.inputSelect(myInput);
   audioShield.volume(0.8);
   audioShield.micGain(0);
-  
+
   while (!Serial) ;
   delay(100);
-  
+
   // Configure the tone detectors with the frequency and number
   // of cycles to match.  These numbers were picked for match
   // times of approx 30 ms.  Longer times are more precise.
@@ -95,7 +98,8 @@ void setup() {
   column2.frequency(1336, 40);
   column3.frequency(1477, 44);
   column4.frequency(1633, 48);
-  
+  startStop.frequency(437, 30);
+
   // Initialize the SD card
   SPI.setMOSI(7);
   SPI.setSCK(14);
@@ -108,8 +112,8 @@ void setup() {
   }
 }
 
-const float row_threshold = 0.2;
-const float column_threshold = 0.2;
+const float row_threshold = 0.1;
+const float column_threshold = 0.1;
 
 void loop() {
   // check if any data has arrived from the serial monitor
@@ -119,17 +123,19 @@ void loop() {
       Serial.println("Record command sent");
       if (mode == 2) stopPlaying();
       if (mode == 0) startRecording();
-    } else if ((c == 's' || c == 'S')) {
+    } 
+    else if ((c == 's' || c == 'S')) {
       Serial.println("Stop command sent");
       if (mode == 1) stopRecording();
       if (mode == 2) stopPlaying();
-    } else if ((c == 'p' || c == 'P')) {
+    } 
+    else if ((c == 'p' || c == 'P')) {
       Serial.println("Play command sent");
       if (mode == 1) stopRecording();
       if (mode == 0) startPlaying();
     }
   }
-  
+
   // If we're playing or recording, carry on...
   if (mode == 1) {
     continueRecording();
@@ -163,83 +169,98 @@ void startRecording() {
 
 // Read in another block of recorded data
 void continueRecording() {
-    float r1, r2, r3, r4, c1, c2, c3, c4;
-    
-    // read all seven tone detectors
-    r1 = row1.read();
-    r2 = row2.read();
-    r3 = row3.read();
-    r4 = row4.read();
-    c1 = column1.read();
-    c2 = column2.read();
-    c3 = column3.read();
-    c4 = column4.read();
-  
-    // print the raw data, for troubleshooting
-  //  Serial.print("tones: ");
-  //  Serial.print(r1);
-  //  Serial.print(", ");
-  //  Serial.print(r2);
-  //  Serial.print(", ");
-  //  Serial.print(r3);
-  //  Serial.print(", ");
-  //  Serial.print(r4);
-  //  Serial.print(",   ");
-  //  Serial.print(c1);
-  //  Serial.print(", ");
-  //  Serial.print(c2);
-  //  Serial.print(", ");
-  //  Serial.print(c3);
-  //  Serial.print(", ");
-  //  Serial.println(c4);
-  
+  float r1, r2, r3, r4, c1, c2, c3, c4;
+
+  // read all seven tone detectors
+  r1 = row1.read();
+  r2 = row2.read();
+  r3 = row3.read();
+  r4 = row4.read();
+  c1 = column1.read();
+  c2 = column2.read();
+  c3 = column3.read();
+  c4 = column4.read();
+
+  // print the raw data, for troubleshooting
+//  Serial.print("tones: ");
+//  Serial.print(r1);
+//  Serial.print(", ");
+//  Serial.print(r2);
+//  Serial.print(", ");
+//  Serial.print(r3);
+//  Serial.print(", ");
+//  Serial.print(r4);
+//  Serial.print(",   ");
+//  Serial.print(c1);
+//  Serial.print(", ");
+//  Serial.print(c2);
+//  Serial.print(", ");
+//  Serial.print(c3);
+//  Serial.print(", ");
+//  Serial.println(c4);
+
   // check all 12 combinations for tone heard
   if (r1 >= row_threshold) {
     if (c1 > column_threshold) {
       digit = '1';
-    } else if (c2 > column_threshold) {
+    } 
+    else if (c2 > column_threshold) {
       digit = '2';
-    } else if (c3 > column_threshold) {
+    } 
+    else if (c3 > column_threshold) {
       digit = '3';
-    } else if (c4 > column_threshold) {
+    } 
+    else if (c4 > column_threshold) {
       digit = 'A';
     }
-  } else if (r2 >= row_threshold) { 
+  } 
+  else if (r2 >= row_threshold) { 
     if (c1 > column_threshold) {
       digit = '4';
-    } else if (c2 > column_threshold) {
+    } 
+    else if (c2 > column_threshold) {
       digit = '5';
-    } else if (c3 > column_threshold) {
+    } 
+    else if (c3 > column_threshold) {
       digit = '6';
-    } else if (c4 > column_threshold) {
+    } 
+    else if (c4 > column_threshold) {
       digit = 'B';
     }
-  } else if (r3 >= row_threshold) { 
+  } 
+  else if (r3 >= row_threshold) { 
     if (c1 > column_threshold) {
       digit = '7';
-    } else if (c2 > column_threshold) {
+    } 
+    else if (c2 > column_threshold) {
       digit = '8';
-    } else if (c3 > column_threshold) {
+    } 
+    else if (c3 > column_threshold) {
       digit = '9';
-    } else if (c4 > column_threshold) {
+    } 
+    else if (c4 > column_threshold) {
       digit = 'C';
     }
-  } else if (r4 >= row_threshold) { 
+  } 
+  else if (r4 >= row_threshold) { 
     if (c1 > column_threshold) {
       digit = 'E';
-    } else if (c2 > column_threshold) {
+    } 
+    else if (c2 > column_threshold) {
       digit = '0';
-    } else if (c3 > column_threshold) {
+    } 
+    else if (c3 > column_threshold) {
       digit = 'F';
-    } else if (c4 > column_threshold) {
+    } 
+    else if (c4 > column_threshold) {
       digit = 'D';
     }
   }
-  
+
   // Check if digit has already been recorded
   if( digit == past ) digit = 0;
-  
-    // print the key, if any found
+
+  // print the key, if any found
   if ((digit > 0)) {
     past = digit;
     frec.write(digit);
@@ -276,53 +297,68 @@ void continuePlaying() {
     if (key == '1') {
       low = 697;
       high = 1209;
-    } else if (key == '2') {
+    } 
+    else if (key == '2') {
       low = 697;
       high = 1336;
-    } else if (key == '3') {
+    } 
+    else if (key == '3') {
       low = 697;
       high = 1477;
-    } else if (key == 'A') {
+    } 
+    else if (key == 'A') {
       low = 697;
       high = 1633;
-    } else if (key == '4') {
+    } 
+    else if (key == '4') {
       low = 770;
       high = 1209;
-    } else if (key == '5') {
+    } 
+    else if (key == '5') {
       low = 770;
       high = 1336;
-    } else if (key == '6') {
+    } 
+    else if (key == '6') {
       low = 770;
       high = 1477;
-    } else if (key == 'B') {
+    } 
+    else if (key == 'B') {
       low = 770;
       high = 11633;
-    } else if (key == '7') {
+    } 
+    else if (key == '7') {
       low = 852;
       high = 1209;
-    } else if (key == '8') {
+    } 
+    else if (key == '8') {
       low = 852;
       high = 1336;
-    } else if (key == '9') {
+    } 
+    else if (key == '9') {
       low = 852;
       high = 1477;
-    } else if (key == 'C') {
+    } 
+    else if (key == 'C') {
       low = 852;
       high = 1633;
-    } else if (key == 'E') {
+    } 
+    else if (key == 'E') {
       low = 941;
       high = 1209;
-    } else if (key == '0') {
+    } 
+    else if (key == '0') {
       low = 941;
       high = 1336;
-    } else if (key == 'F') {
+    } 
+    else if (key == 'F') {
       low = 941;
       high = 1477;
-    } else if (key == 'D') {
+    } 
+    else if (key == 'D') {
       low = 941;
       high = 1633;
     }
-    
+
     // Play the DTMF tone specified by the file
     if (low > 0 && high > 0) {
       Serial.print("Output sound for key ");
@@ -358,5 +394,6 @@ void stopPlaying() {
   if (mode == 2) frec.close();
   mode = 0;
 }
+
 
 
