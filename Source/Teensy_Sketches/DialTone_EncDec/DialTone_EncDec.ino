952543051b6af844e30b3c839e2dcff22b9d51f7
sricha16 <s.richard990@gmail.com>
2015-02-15 15:31:38 -0700

    Added Start/Stop Signals
    -Added signals to start recording, start playing, and stop current
    process
    -updated if-else statement to case statement
    -started work on file naming convention

--- Source/Teensy_Sketches/DialTone_EncDec/DialTone_EncDec.ino
+++ Source/Teensy_Sketches/DialTone_EncDec/DialTone_EncDec.ino
@@ -1,8 +1,7 @@
-// Dial Tone (DTMF) decoding example.
+// Dial Tone (DTMF) encoding decoding example.
 //
-// The audio with dial tones is connected to audio shield
-// Left Line-In pin.  Dial tone output is produced on the
-// Line-Out and headphones.
+// The signals are read in through the mic jack and played
+// back out through the headphone jack
 //
 // Use the Arduino Serial Monitor to watch for incoming
 // dial tones, and to send digits to be played as dial tones.
@@ -19,9 +18,6 @@
 // order data flows, inputs/sources -> processing -> outputs
 //
 AudioInputI2S            audioIn;
-AudioAnalyzePeak         peak;          //xy=278,108
-AudioRecordQueue         queue;         //xy=281,63
-AudioPlaySdRaw           playRaw;       //xy=302,157
 AudioAnalyzeToneDetect   row1;     // 7 tone detectors are needed
 AudioAnalyzeToneDetect   row2;     // to receive DTMF dial tones
 AudioAnalyzeToneDetect   row3;
@@ -30,7 +26,9 @@ AudioAnalyzeToneDetect   column1;
 AudioAnalyzeToneDetect   column2;
 AudioAnalyzeToneDetect   column3;
 AudioAnalyzeToneDetect   column4;
-AudioAnalyzeToneDetect   startStop;
+AudioAnalyzeToneDetect   rec;      // Signal to start recording
+AudioAnalyzeToneDetect   ply;      // Signal to start playing
+AudioAnalyzeToneDetect   stp;      // Signal to stop playing or recording
 AudioSynthWaveformSine   sine1;    // 2 sine waves
 AudioSynthWaveformSine   sine2;    // to create DTMF
 AudioMixer4              mixer;
@@ -46,16 +44,15 @@ AudioConnection patchCord05(audioIn, 0, column1, 0);
 AudioConnection patchCord06(audioIn, 0, column2, 0);
 AudioConnection patchCord07(audioIn, 0, column3, 0);
 AudioConnection patchCord08(audioIn, 0, column4, 0);
-AudioConnection patchCord09(audioIn, 0, startStop, 0);
-AudioConnection patchCord10(sine1, 0, mixer, 0);
-AudioConnection patchCord11(sine2, 0, mixer, 1);
-AudioConnection patchCord12(mixer, 0, audioOut, 0);
-AudioConnection patchCord13(mixer, 0, audioOut, 1);
-// For Recording and SD Card
-AudioConnection patchCord21(audioIn, 0, queue, 0);
-AudioConnection patchCord22(audioIn, 0, peak, 0);
-AudioConnection patchCord23(playRaw, 0, audioOut, 0);
-AudioConnection patchCord34(playRaw, 0, audioOut, 1);
+// For Start/Stop Signals
+AudioConnection patchCord09(audioIn, 0, rec, 0);
+AudioConnection patchCord10(audioIn, 0, ply, 0);
+AudioConnection patchCord11(audioIn, 0, stp, 0);
+// For Output
+AudioConnection patchCord12(sine1, 0, mixer, 0);
+AudioConnection patchCord13(sine2, 0, mixer, 1);
+AudioConnection patchCord14(mixer, 0, audioOut, 0);
+AudioConnection patchCord15(mixer, 0, audioOut, 1);
 
 // Create an object to control the audio shield.
 AudioControlSGTL5000 audioShield;
@@ -69,9 +66,11 @@ int mode = 0;  // 0=stopped, 1=recording, 2=playing
 // The file where data is stored
 File frec;
 
-// Objects for recording
-char digit = 0;
-char past = 0;
+// Unique filename to store data under
+char * filename;
+
+// On board LED
+int led = 13;
 
 void setup() {
   // Audio connections require memory to work.  For more
@@ -81,7 +80,7 @@ void setup() {
   // Enable the audio shield and set the output volume.
   audioShield.enable();
   audioShield.inputSelect(myInput);
-  audioShield.volume(0.8);
+  audioShield.volume(0.5);
   audioShield.micGain(0);
 
   while (!Serial) ;
@@ -90,15 +89,17 @@ void setup() {
   // Configure the tone detectors with the frequency and number
   // of cycles to match.  These numbers were picked for match
   // times of approx 30 ms.  Longer times are more precise.
-  row1.frequency(697, 21);
-  row2.frequency(770, 23);
-  row3.frequency(852, 25);
-  row4.frequency(941, 28);
-  column1.frequency(1209, 36);
-  column2.frequency(1336, 40);
-  column3.frequency(1477, 44);
-  column4.frequency(1633, 48);
-  startStop.frequency(437, 30);
+  row1.frequency(697, 21);  // 30.1291 ms
+  row2.frequency(770, 23);  // 29.8701 ms
+  row3.frequency(852, 25);  // 29.3427 ms
+  row4.frequency(941, 28);  // 29.7556 ms
+  column1.frequency(1209, 36);  // 29.7767 ms
+  column2.frequency(1336, 40);  // 29.9401 ms
+  column3.frequency(1477, 44);  // 29.7901 ms
+  column4.frequency(1633, 48);  // 29.3938 ms
+  rec.frequency(1951, 59);  // 30.2409 ms 1951
+  ply.frequency(2097, 63);  // 30.0429 ms 2097
+  stp.frequency(2229, 67);  // 30.0583 ms 2229
 
   // Initialize the SD card
   SPI.setMOSI(7);
@@ -110,31 +111,45 @@ void setup() {
       delay(500);
     }
   }
+  
+// pinMode method breaks read() function on tones... have no idea why...
+// actually it might be because the mic port also uses pin 13 so they fight over it
+  // Initialize LED
+//  pinMode(led, OUTPUT);
+//  digitalWrite(led, LOW);
 }
 
-const float row_threshold = 0.1;
-const float column_threshold = 0.1;
+const float tone_threshold = 0.1;
 
 void loop() {
-  // check if any data has arrived from the serial monitor
-  if (Serial.available()) {
-    char c = Serial.read();
-    if ((c == 'r' || c == 'R')) {
-      Serial.println("Record command sent");
-      if (mode == 2) stopPlaying();
-      if (mode == 0) startRecording();
-    } 
-    else if ((c == 's' || c == 'S')) {
-      Serial.println("Stop command sent");
-      if (mode == 1) stopRecording();
-      if (mode == 2) stopPlaying();
-    } 
-    else if ((c == 'p' || c == 'P')) {
-      Serial.println("Play command sent");
-      if (mode == 1) stopRecording();
-      if (mode == 0) startPlaying();
-    }
+  float r, p, s;
+  
+  // read for start-stop and playData signals
+  r = rec.read();
+  p = ply.read();
+  s = stp.read();
+  
+//  Serial.print(r);
+//  Serial.print(", ");
+//  Serial.print(p);
+//  Serial.print(", ");
+//  Serial.println(s); 
+  
+  // compare to threshold
+  if( r >= tone_threshold ) {
+    // start recording if in wait mode
+    if( mode == 0 ) startRecording();
   }
+  if( p >= tone_threshold ){
+    // start playing if in wait mode
+    if( mode == 0 ) startPlaying();
+  }
+  if( s >= tone_threshold ){
+    // stop recording if recording
+    if( mode == 1 ) stopRecording();
+    // stop playing if playing
+    if( mode == 2 ) stopPlaying();
+  }  
 
   // If we're playing or recording, carry on...
   if (mode == 1) {
@@ -149,27 +164,31 @@ void loop() {
 
 // Initial steps required to record
 void startRecording() {
+  // Signify the start of recording via serial output and turning LED on
   Serial.println("startRecording");
+  //digitalWrite(led, HIGH);
+  
+  // Check if file already exists, delete it if it does
   if (SD.exists("test.RAW")) {
-    // The SD library writes new data to the end of the
-    // file, so to start a new recording, the old file
-    // must be deleted before new data is written.
     SD.remove("test.RAW");
   }
+  
+  // Open the file
   frec = SD.open("test.RAW", FILE_WRITE);
+  
+  // Check that the file was open correctly
   if (frec) {
     mode = 1;
-    digit = 0;
-    past = 0;
   } 
   else {
-    Serial.println("File could not be opened");
+    Serial.println("File could not be opened for recording");
   }
 }
 
 // Read in another block of recorded data
 void continueRecording() {
   float r1, r2, r3, r4, c1, c2, c3, c4;
+  char digit = 0;
 
   // read all seven tone detectors
   r1 = row1.read();
@@ -181,7 +200,7 @@ void continueRecording() {
   c3 = column3.read();
   c4 = column4.read();
 
-  // print the raw data, for troubleshooting
+//  // print the raw data, for troubleshooting
 //  Serial.print("tones: ");
 //  Serial.print(r1);
 //  Serial.print(", ");
@@ -200,69 +219,65 @@ void continueRecording() {
 //  Serial.println(c4);
 
   // check all 12 combinations for tone heard
-  if (r1 >= row_threshold) {
-    if (c1 > column_threshold) {
+  if (r1 >= tone_threshold) {
+    if (c1 > tone_threshold) {
       digit = '1';
     } 
-    else if (c2 > column_threshold) {
+    else if (c2 > tone_threshold) {
       digit = '2';
     } 
-    else if (c3 > column_threshold) {
+    else if (c3 > tone_threshold) {
       digit = '3';
     } 
-    else if (c4 > column_threshold) {
+    else if (c4 > tone_threshold) {
       digit = 'A';
     }
   } 
-  else if (r2 >= row_threshold) { 
-    if (c1 > column_threshold) {
+  else if (r2 >= tone_threshold) { 
+    if (c1 > tone_threshold) {
       digit = '4';
     } 
-    else if (c2 > column_threshold) {
+    else if (c2 > tone_threshold) {
       digit = '5';
     } 
-    else if (c3 > column_threshold) {
+    else if (c3 > tone_threshold) {
       digit = '6';
     } 
-    else if (c4 > column_threshold) {
+    else if (c4 > tone_threshold) {
       digit = 'B';
     }
   } 
-  else if (r3 >= row_threshold) { 
-    if (c1 > column_threshold) {
+  else if (r3 >= tone_threshold) { 
+    if (c1 > tone_threshold) {
       digit = '7';
     } 
-    else if (c2 > column_threshold) {
+    else if (c2 > tone_threshold) {
       digit = '8';
     } 
-    else if (c3 > column_threshold) {
+    else if (c3 > tone_threshold) {
       digit = '9';
     } 
-    else if (c4 > column_threshold) {
+    else if (c4 > tone_threshold) {
       digit = 'C';
     }
   } 
-  else if (r4 >= row_threshold) { 
-    if (c1 > column_threshold) {
+  else if (r4 >= tone_threshold) { 
+    if (c1 > tone_threshold) {
       digit = 'E';
     } 
-    else if (c2 > column_threshold) {
+    else if (c2 > tone_threshold) {
       digit = '0';
     } 
-    else if (c3 > column_threshold) {
+    else if (c3 > tone_threshold) {
       digit = 'F';
     } 
-    else if (c4 > column_threshold) {
+    else if (c4 > tone_threshold) {
       digit = 'D';
     }
   }
 
-  // Check if digit has already been recorded
-  if( digit == past ) digit = 0;
-
-  // print the key, if any found
+  // print the key, if any found437
   if ((digit > 0)) {
-    past = digit;
     frec.write(digit);
     Serial.print("  --> Key: ");
     Serial.print(digit);
@@ -272,18 +287,43 @@ void continueRecording() {
 
 // Stop recording and close the file
 void stopRecording() {
+  // Signify stop recording by serial output and turning LED off
   Serial.println("stopRecording");
+  //digitalWrite(led, LOW);
+  
+  // Close the file and update the mode
   if (mode == 1) {
     frec.close();
+    frec = SD.open("test.RAW");
+    if(frec){
+      if (frec.available()) {
+        for( int i = 0; i < 5; i++){
+          filename = filename + frec.read();
+        }
+        Serial.print("File saved as ");
+        Serial.println(filename);
+      }
+    }
   }
   mode = 0;
 }
 
 // Initial steps needed to play the file
 void startPlaying() {
+  // Signify start playing by serial output and turning LED on
   Serial.println("startPlaying");
+  //digitalWrite(led, HIGH);
+  
+  // Open file
   frec = SD.open("test.RAW");
-  mode = 2;
+  
+  // Check if file opened correctly and update mode
+  if (frec) {
+    mode = 2;
+  } 
+  else {
+    Serial.println("File could not be opened for playing");
+  }
 }
 
 // Check if the end has been reached, stop if yes continue if no
@@ -294,69 +334,71 @@ void continuePlaying() {
     // Read in a value from the file
     char key = frec.read();
     // Match the character to the correct tones
-    if (key == '1') {
-      low = 697;
-      high = 1209;
-    } 
-    else if (key == '2') {
-      low = 697;
-      high = 1336;
-    } 
-    else if (key == '3') {
-      low = 697;
-      high = 1477;
-    } 
-    else if (key == 'A') {
-      low = 697;
-      high = 1633;
-    } 
-    else if (key == '4') {
-      low = 770;
-      high = 1209;
-    } 
-    else if (key == '5') {
-      low = 770;
-      high = 1336;
-    } 
-    else if (key == '6') {
-      low = 770;
-      high = 1477;
-    } 
-    else if (key == 'B') {
-      low = 770;
-      high = 11633;
-    } 
-    else if (key == '7') {
-      low = 852;
-      high = 1209;
-    } 
-    else if (key == '8') {
-      low = 852;
-      high = 1336;
-    } 
-    else if (key == '9') {
-      low = 852;
-      high = 1477;
-    } 
-    else if (key == 'C') {
-      low = 852;
-      high = 1633;
-    } 
-    else if (key == 'E') {
-      low = 941;
-      high = 1209;
-    } 
-    else if (key == '0') {
-      low = 941;
-      high = 1336;
-    } 
-    else if (key == 'F') {
-      low = 941;
-      high = 1477;
-    } 
-    else if (key == 'D') {
-      low = 941;
-      high = 1633;
+    switch( key ){
+      case '1' :
+        low = 697;
+        high = 1209;
+        break;
+      case '2' :
+        low = 697;
+        high = 1336;
+        break;
+      case '3' :
+        low = 697;
+        high = 1477;
+        break;
+      case '4' :
+        low = 770;
+        high = 1209;
+        break;
+      case '5' :
+        low = 770;
+        high = 1336;
+        break;
+      case '6' :
+        low = 770;
+        high = 1477;
+        break;
+      case '7' :
+        low = 852;
+        high = 1209;
+        break;
+      case '8' :
+        low = 852;
+        high = 1336;
+        break;
+      case '9' :
+        low = 852;
+        high = 1477;
+        break;
+      case '0' :
+        low = 941;
+        high = 1336;
+        break;
+      case 'A' :
+        low = 697;
+        high = 1633;
+        break;
+      case 'B' :
+        low = 770;
+        high = 11633;
+        break;
+      case 'C' :
+        low = 852;
+        high = 1633;
+        break;
+      case 'D' :
+        low = 941;
+        high = 1633;
+        break;
+      case 'E' :
+        low = 941;
+        high = 1209;
+        break;
+      case 'F' :
+        low = 941;
+        high = 1477;
+        break;
     }
 
     // Play the DTMF tone specified by the file
@@ -390,7 +432,11 @@ void continuePlaying() {
 
 // Stop playing the file
 void stopPlaying() {
+  // Signify stop playing by serial output and turning LED off
   Serial.println("stopPlaying");
+  //digitalWrite(led, LOW);
+  
+  // Close file and update mode
   if (mode == 2) frec.close();
   mode = 0;
 }
